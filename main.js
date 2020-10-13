dcd80a3f8cabf2303a12f2f11e7f934f3e2d6ffc
mauricio <maurifazzano@gmail.com>
2020-10-13 18:25:35 -0300

    Funcion para mostrar los libros en la pagina con javascript

--- main.js
+++ main.js
@@ -1,4 +1,57 @@
-let myLibrary = [];
+let myLibrary = [{
+    title: 'Alfreds Futterkiste',
+    author: 'Maria Anders',
+    pages: '123',
+    read: true
+},
+{
+    title: 'Ambar Futterkiste',
+    author: 'Maria Anders',
+    pages: '222',
+    read: false
+},
+{
+    title: 'Sota Futterkiste',
+    author: 'Maria Anders',
+    pages: '123',
+    read: true
+},
+{
+    title: 'Sota Futterkiste',
+    author: 'Maria Anders',
+    pages: '123',
+    read: true
+},
+{
+    title: 'Sota Futterkiste',
+    author: 'Maria Anders',
+    pages: '123',
+    read: true
+},
+{
+    title: 'Sota Futterkiste',
+    author: 'Maria Anders',
+    pages: '123',
+    read: true
+},
+{
+    title: 'Sota Futterkiste',
+    author: 'Maria Anders',
+    pages: '123',
+    read: true
+},
+{
+    title: 'Sota Futterkiste',
+    author: 'Maria Anders',
+    pages: '123',
+    read: true
+},
+{
+    title: 'Sota Futterkiste',
+    author: 'Maria Anders',
+    pages: '123',
+    read: true
+}];
 
 function Book(title, author, pages, read){
     this.title = title;
@@ -11,8 +64,29 @@ function Book(title, author, pages, read){
 }
 
 function addBookToLibrary(book) {
-
     myLibrary.push(book);
 }
 
+function displayBooksOnPage(library){
 
+    const table = document.querySelector('tbody');
+
+    library.forEach(book => {
+        console.log(book);
+        const row = document.createElement('tr');
+
+        for(const property in book){
+            let cell = document.createElement('td');
+
+            if(property === 'read')
+                book[property] ? cell.innerText = 'Yes' : cell.innerText = 'No';
+            else
+                cell.innerText = book[property];
+
+            row.appendChild(cell);
+        }
+        table.appendChild(row);
+    });
+}
+
+displayBooksOnPage(myLibrary);
